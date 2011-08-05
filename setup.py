a4fa6f061204fcd1039b73e475b48e1d56053be7
Yamashita Yuu <yamashita@geishatokyo.com>
2011-08-05 18:30:49 +0900

    updated setup.py for PyPi registration.

--- setup.py
+++ setup.py
@@ -10,12 +10,26 @@ except ImportError:
 setup(
   name='fabric_deploy',
   version='0.0.1',
-  description='capistrano like deploy recipe for fabric',
   author='Yamashita Yuu',
   author_email='yamashita@geishatokyo.com',
-  url='http://www.geishatokyo.com/',
+  description='capistrano like deploy recipe for Fabric',
+  license='BSD',
+  keywords='fabric deploy',
+  url='https://github.com/yyuu/fabric_deploy',
+  long_description="""Capistrano like deploy recipe for Fabric.""",
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: System :: Software Distribution',
+    'Topic :: System :: Systems Administration',
+    'License :: OSI Approved :: BSD License',
+  ],
+  platforms=[
+    'POSIX'
+  ],
   install_requires=[
-    'Fabric>=1.2',
+    'Fabric',
   ],
   packages=find_packages(),
 )
